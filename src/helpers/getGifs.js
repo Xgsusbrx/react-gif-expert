6106a471976899509c3b6c34e5fa137379d95a2a
Brian <brian95gsus@gmail.com>
2025-07-06 14:30:15 -0400

    fix: corrige peticion de api

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -4,7 +4,7 @@
 //funcion que llama a la api y usa el estado category como parametro de busqueda con una limitacion de 10 repuestas
  export const getGifs = async (category) => {
     const url =
-      "https://api.giphy.com/v1/gifs/search?api_key=5px0kaluR36y1op7f9EUsjYuvEMdaw7j&q=${category}&limit=10";
+      `https://api.giphy.com/v1/gifs/search?api_key=5px0kaluR36y1op7f9EUsjYuvEMdaw7j&q=${category}&limit=10`;
     const resp = await fetch(url);
     //desestructuracion de la respuesta 
     const {data} = await resp.json()
